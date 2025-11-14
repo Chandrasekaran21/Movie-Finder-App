c4e284d7c14d698226962c67c6d7298754f2c214
Chandrasekaran21 <schandru2115@gmail.com>
2025-11-14 23:34:50 +0530

    Updated gitignore and remove showing API key

--- src/utils/Options.jsx
+++ src/utils/Options.jsx
@@ -3,6 +3,6 @@ export const options = {
             method: 'GET',
             headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjgwNjU3YjZlOGMxZGFmNDM1MTZhZTEwYmRkNDU5YSIsIm5iZiI6MTc2MTc1NzgzMy41NDgsInN1YiI6IjY5MDI0YTg5YmE4ZThhNTFjM2EyYzBmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i7g8hh3yknrppZPRwJe8Fd0ng4YKZk2vozWfpjy8SHU'
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
             }
         };
